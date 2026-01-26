0178081c747cf803effe935e76eeaf0f54fde920
nlund3 <42989847+nlund3@users.noreply.github.com>
2026-01-26 11:23:24 -0700

    Set pins in My_HAL_GPIO_Init function

--- lab1/Src/hal_gpio.c
+++ lab1/Src/hal_gpio.c
@@ -1,11 +1,21 @@
 #include <stdint.h>
 #include <stm32f0xx_hal.h>
 #include <stm32f0xx_hal_gpio.h>
-/*
+
 void My_HAL_GPIO_Init(GPIO_TypeDef  *GPIOx, GPIO_InitTypeDef *GPIO_Init)
 {
+    // Configure LED pins (PC6-PC9)
+    GPIOC->MODER = 0x55000; // General purpose output mode
+    GPIOC->OTYPER = 0x0;    // Push-pull output type
+    GPIOC->OSPEEDR = 0x0;   // Low speed
+    GPIOC->PUPDR = 0x0;     // No pull-up or pull-down
+
+    // Configure USER button pin PA0
+    GPIOA->MODER = 0x0;     // Digital input mode
+    GPIOA->OSPEEDR = 0x0;   // Low speed
+    GPIOA->PUPDR = 0x2;     // Pull-down resistor
 }
-*/
+
 
 /*
 void My_HAL_GPIO_DeInit(GPIO_TypeDef  *GPIOx, uint32_t GPIO_Pin)
