5aef11226d5fec8ab9de8b5e19bb5774910d4bde
nlund3 <42989847+nlund3@users.noreply.github.com>
2026-01-26 08:24:57 -0700

    Add example blinky code from the lab handout

--- lab1/Src/lab1.c
+++ lab1/Src/lab1.c
@@ -9,16 +9,27 @@ void SystemClock_Config(void);
   */
 int main(void)
 {
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
-  /* Configure the system clock */
-  SystemClock_Config();
+    HAL_Init(); // Reset of all peripherals, init the Flash and Systick
+    SystemClock_Config(); // Configure the system clock
 
-  while (1)
-  {
- 
-  }
-  return -1;
+    /* This example uses HAL library calls to control
+     * the GPIOC peripheral. You'll be redoing this code
+     * with hardware register access. */
+
+    __HAL_RCC_GPIOC_CLK_ENABLE(); // Enable the GPIOC clock in the RCC
+
+    // Set up a configuration struct to pass to the initialization function
+    GPIO_InitTypeDef initStr = {GPIO_PIN_8 | GPIO_PIN_9,
+                                GPIO_MODE_OUTPUT_PP,
+                                GPIO_SPEED_FREQ_LOW,
+                                GPIO_NOPULL};
+    HAL_GPIO_Init(GPIOC, &initStr); // Initialize pins PC8 and PC9
+    HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_SET); // Start PC8 high
+    while (1) {
+        HAL_Delay(200); // Delay 200ms
+        // Toggle the output state of both PC8 and PC9
+        HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_8 | GPIO_PIN_9);
+    }
 }
 
 /**
