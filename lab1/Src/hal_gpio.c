44848445514b7f20f236405c3638810135ffb9be
nlund3 <42989847+nlund3@users.noreply.github.com>
2026-01-30 12:45:35 -0700

    Read Pin implemented

--- lab1/Src/hal_gpio.c
+++ lab1/Src/hal_gpio.c
@@ -18,9 +18,10 @@ void My_HAL_GPIO_Init(GPIO_TypeDef  *GPIOx, GPIO_InitTypeDef *GPIO_Init)
 }
 
 
-/*GPIO_PinState My_HAL_GPIO_ReadPin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin)
+GPIO_PinState My_HAL_GPIO_ReadPin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin)
 {
-}*/
+    return GPIOx->IDR & GPIO_Pin;
+}
 
 
 void My_HAL_GPIO_WritePin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState)
